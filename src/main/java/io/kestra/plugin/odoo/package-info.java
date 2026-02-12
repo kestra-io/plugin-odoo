8725e0df361aad1c5e05af7d2ecf4dab63567c6b
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 11:31:05 +0100

    chore: merge package categories attribute

--- src/main/java/io/kestra/plugin/odoo/package-info.java
+++ src/main/java/io/kestra/plugin/odoo/package-info.java
@@ -1,5 +1,4 @@
-@PluginSubGroup(title = "Odoo", description = "Integrate with Odoo ERP system via XML-RPC API for data orchestration and workflow automation.", categories = PluginSubGroup.PluginCategory.TOOL,
-    categories = { PluginSubGroup.PluginCategory.BUSINESS }
+@PluginSubGroup(title = "Odoo", description = "Integrate with Odoo ERP system via XML-RPC API for data orchestration and workflow automation.",categories = { PluginSubGroup.PluginCategory.BUSINESS }
 )
 package io.kestra.plugin.odoo;
 
