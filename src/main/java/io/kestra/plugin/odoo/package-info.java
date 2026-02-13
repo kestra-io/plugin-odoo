2d7c6fddd16caf06a759c99a616474cdc2f7e7f5
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:33:42 +0100

    chore: update plugin categories (#19)

--- src/main/java/io/kestra/plugin/odoo/package-info.java
+++ src/main/java/io/kestra/plugin/odoo/package-info.java
@@ -1,4 +1,5 @@
-@PluginSubGroup(title = "Odoo", description = "Integrate with Odoo ERP system via XML-RPC API for data orchestration and workflow automation.", categories = PluginSubGroup.PluginCategory.TOOL)
+@PluginSubGroup(title = "Odoo", description = "Integrate with Odoo ERP system via XML-RPC API for data orchestration and workflow automation.",categories = { PluginSubGroup.PluginCategory.BUSINESS }
+)
 package io.kestra.plugin.odoo;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
